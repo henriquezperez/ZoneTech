c6576820a61210cfc1c04e1a21b54e8aa188b1e3
DenilsonLic <100452083+DenilsonLic@users.noreply.github.com>
2022-05-15 13:37:33 -0600

    loginYregistro

---
+++ ZoneTech/Controllers/AccesoController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ZoneTech.Models;
+
+namespace ZoneTech.Controllers
+{
+    public class AccesoController : Controller
+    {
+        public IActionResult Login()
+        {
+
+            return View();
+        }
+
+        public ActionResult Registrar()
+        {
+            return View();
+        }
+    }
+}
--- ZoneTech/Program.cs
+++ ZoneTech/Program.cs
@@ -3,16 +3,17 @@ using ZoneTech.Data;
 
 
 var builder = WebApplication.CreateBuilder(args);
+
 //16/abril/2022
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-builder.Services.AddDbContext<ApplicationDBContext>(options =>options.UseSqlServer(@"Server=MIGUEL-TOSHIBA\SQLEXPRESS01;Database=ZoneTechProyect; TrustServerCertificate=true; Trusted_Connection=true; Connection Timeout= 30; Integrated Security=true; Persist Security Info= false; Encrypt= true; MultipleActiveResultSets=true;"));
+builder.Services.AddDbContext<ApplicationDBContext>(options =>options.UseSqlServer(@"Server=DENILSONPC;Database=ZoneTechProyect; TrustServerCertificate=true; Trusted_Connection=true; Connection Timeout= 30; Integrated Security=true; Persist Security Info= false; Encrypt= true; MultipleActiveResultSets=true;"));
 
 //builder.Services.AddTransient<IEstado, EstadoRE>();
 
-
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
