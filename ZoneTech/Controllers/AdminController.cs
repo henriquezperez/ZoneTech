833f63a26478e66fac94836521bf857292d26162
Miguel <hp0826022019@unab.edu.sv>
2022-05-24 21:23:08 -0600

    update

---
+++ ZoneTech/Controllers/AdminController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ZoneTech.Data;
+using ZoneTech.Models;
+
+namespace ZoneTech.Controllers
+{
+    public class AdminController : Controller
+    {
+         private ApplicationDBContext db;
+        private readonly ILogger<AdminController> _logger;
+        public AdminController(ILogger<AdminController> logger, ApplicationDBContext _db){
+            _logger = logger;
+            db = _db;
+        }
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Articulos(){
+            var lista = db.ArticuloTBL.ToList();
+            ViewBag.list = lista;
+            return View();
+        }
+
+        public IActionResult InsertArticulo(ArticuloML art){
+            db.ArticuloTBL.Add(art);
+            return View("Articulos");
+        }
+
+        public IActionResult Articulonuevo(){
+            var list = db.MarcaTBL.ToList();
+            var listCa = db.CategoriaTBL.ToList();
+            var listMo = db.ModeloTBL.ToList();
+
+            ViewBag.listMarca = list;
+            ViewBag.listCategoria = listCa;
+            ViewBag.listModelo = listMo;
+
+
+            return View();
+        }
+    }
+}
--- ZoneTech/Controllers/HomeController.cs
+++ ZoneTech/Controllers/HomeController.cs
@@ -1,18 +1,25 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using ZoneTech.Data;
 using ZoneTech.Models;
 
 namespace ZoneTech.Controllers
 {
     public class HomeController : Controller
     {
-        
+        private ApplicationDBContext db;
+        private readonly ILogger<HomeController> _logger;
+        public HomeController(ILogger<HomeController> logger, ApplicationDBContext _db){
+            _logger = logger;
+            db = _db;
+        }
 
         public IActionResult Index()
         {
+            var cat = db.CategoriaTBL.ToList();
+            ViewBag.categorias = cat;
             return View();
         }
-
-        
+ 
     }
 }
